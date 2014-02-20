d30aec811c570d80d594886c5b68152c602a86ab
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2014-02-21 01:24:11 +0900

    uuu

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module ShoppingRails
     config.before_configuration do
       I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
       I18n.locale = :ja
+      I18n.enforce_available_locales = true
       I18n.default_locale = :ja
       config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
       config.i18n.locale = :ja
